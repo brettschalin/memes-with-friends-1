bc49a48e2671c11746c194081fa6886d77412c36
Tomasz Kramkowski <tk@the-tk.com>
2017-06-05 23:32:12 +0100

    Card.cpp: Make ::draw readable
    As it stood, Card::draw was a bit of a mess. By splitting the parameters
    across multiple lines, it's now much easier to understand what is
    happening.

--- Memes With Friends/Memes With Friends/Card.cpp
+++ Memes With Friends/Memes With Friends/Card.cpp
@@ -46,26 +46,67 @@ void Card::draw()
 	if (!this->meme_image) return;
 
 	// Draw the meme image first at the appropriate location
-	al_draw_scaled_bitmap(this->meme_image, 0, 0, al_get_bitmap_width(this->meme_image), al_get_bitmap_height(this->meme_image), this->x1, this->y1, Card::CARD_W, Card::CARD_H, 0);
+	al_draw_scaled_bitmap(this->meme_image,
+			      0, 0,
+			      al_get_bitmap_width(this->meme_image),
+			      al_get_bitmap_height(this->meme_image),
+			      this->x1, this->y1,
+			      Card::CARD_W, Card::CARD_H, 0);
 
 	// Draw the border around the meme image to make it a card
-	al_draw_rectangle(this->x1, this->y1, this->x2, this->y2, this->CARD_COLOR, Card::CARD_BORDER_WIDTH);
+	al_draw_rectangle(this->x1,
+			  this->y1,
+			  this->x2,
+			  this->y2,
+			  this->CARD_COLOR, Card::CARD_BORDER_WIDTH);
 
 	// Draw left number
-	al_draw_filled_rectangle(this->x1 + (this->CARD_BORDER_WIDTH * 2) - 5, this->y1 + (this->CARD_H / 2) - 5, this->x1 + (this->CARD_BORDER_WIDTH * 2) + 15, this->y1 + (this->CARD_H / 2) + 15, this->gamedisplay->get_background_color());
-	al_draw_text(font, al_map_rgb(0, 0, 0), this->x1 + (this->CARD_BORDER_WIDTH * 2), this->y1 + (this->CARD_H / 2), ALLEGRO_ALIGN_LEFT, std::to_string(left).c_str());
+	al_draw_filled_rectangle(this->x1 + (this->CARD_BORDER_WIDTH * 2) - 5,
+				 this->y1 + (this->CARD_H / 2) - 5,
+				 this->x1 + (this->CARD_BORDER_WIDTH * 2) + 15,
+				 this->y1 + (this->CARD_H / 2) + 15,
+				 this->gamedisplay->get_background_color());
+	al_draw_text(font, al_map_rgb(0, 0, 0),
+		     this->x1 + (this->CARD_BORDER_WIDTH * 2),
+		     this->y1 + (this->CARD_H / 2),
+		     ALLEGRO_ALIGN_LEFT,
+		     std::to_string(left).c_str());
 
 	// Draw right number
-	al_draw_filled_rectangle(this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) - 15, this->y1 + (this->CARD_H / 2) - 5, this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) + 5, this->y1 + (this->CARD_H / 2) + 15, this->gamedisplay->get_background_color());
-	al_draw_text(font, al_map_rgb(0, 0, 0), this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) - 10, this->y1 + (this->CARD_H / 2), ALLEGRO_ALIGN_LEFT, std::to_string(right).c_str());
+	al_draw_filled_rectangle(this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) - 15,
+				 this->y1 + (this->CARD_H / 2) - 5,
+				 this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) + 5,
+				 this->y1 + (this->CARD_H / 2) + 15,
+				 this->gamedisplay->get_background_color());
+	al_draw_text(font, al_map_rgb(0, 0, 0),
+		     this->x1 + this->CARD_W - (this->CARD_BORDER_WIDTH * 2) - 10,
+		     this->y1 + (this->CARD_H / 2),
+		     ALLEGRO_ALIGN_LEFT,
+		     std::to_string(right).c_str());
 
 	// Draw up number
-	al_draw_filled_rectangle(this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) - 5, this->y1 + (this->CARD_BORDER_WIDTH * 2) - 5, this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) + 15, this->y1 + (this->CARD_BORDER_WIDTH * 2) + 15, this->gamedisplay->get_background_color());
-	al_draw_text(font, al_map_rgb(0, 0, 0), this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2), this->y1 + (this->CARD_BORDER_WIDTH * 2), ALLEGRO_ALIGN_LEFT, std::to_string(up).c_str());
+	al_draw_filled_rectangle(this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) - 5,
+				 this->y1 + (this->CARD_BORDER_WIDTH * 2) - 5,
+				 this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) + 15,
+				 this->y1 + (this->CARD_BORDER_WIDTH * 2) + 15,
+				 this->gamedisplay->get_background_color());
+	al_draw_text(font, al_map_rgb(0, 0, 0),
+		     this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2),
+		     this->y1 + (this->CARD_BORDER_WIDTH * 2),
+		     ALLEGRO_ALIGN_LEFT,
+		     std::to_string(up).c_str());
 
 	// Draw down number
-	al_draw_filled_rectangle(this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) - 5, this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) - 15, this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) + 15, this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) + 5, this->gamedisplay->get_background_color());
-	al_draw_text(font, al_map_rgb(0, 0, 0), this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2), this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) - 10, ALLEGRO_ALIGN_LEFT, std::to_string(down).c_str());
+	al_draw_filled_rectangle(this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) - 5,
+				 this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) - 15,
+				 this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2) + 15,
+				 this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) + 5,
+				 this->gamedisplay->get_background_color());
+	al_draw_text(font, al_map_rgb(0, 0, 0),
+		     this->x1 + (this->CARD_W / 2) - (this->CARD_BORDER_WIDTH * 2),
+		     this->y1 + this->CARD_H - (this->CARD_BORDER_WIDTH * 2) - 10,
+		     ALLEGRO_ALIGN_LEFT,
+		     std::to_string(down).c_str());
 }
 
 void Card::set_pos(int x1, int y1)
